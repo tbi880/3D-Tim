6f4fafa904bbabcec8df75abf47381e500d6ad70
tianyuanbi <tbi880@aucklanduni.ac.nz>
2024-11-24 04:00:55 +1300

    bug fixed for graphic setting

--- src/sharedContexts/GraphicSettingProvider.jsx
+++ src/sharedContexts/GraphicSettingProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { getUserDisableUnnecessaryComponentAnimation, getUserDpr, getUserAntialias } from "../pages/Status";
 
 export const graphicSettingContext = createContext();
@@ -7,6 +7,11 @@ export const GraphicSettingProvider = ({ children }) => {
     const [antialias, setAntialias] = useState(() => getUserAntialias() ?? (isPortraitPhoneScreen ? false : true));
     const [disableUnnecessaryComponentAnimation, setDisableUnnecessaryComponentAnimation] = useState(() => getUserDisableUnnecessaryComponentAnimation() ?? (window.innerHeight > window.innerWidth ? true : false));
 
+    useEffect(() => {
+        setDpr(getUserDpr());
+        setAntialias(getUserAntialias());
+        setDisableUnnecessaryComponentAnimation(getUserDisableUnnecessaryComponentAnimation());
+    }, []);
 
     return (
         <graphicSettingContext.Provider value={{ dpr, setDpr, antialias, setAntialias, disableUnnecessaryComponentAnimation, setDisableUnnecessaryComponentAnimation }}>
