a31a333a53da7f7aa5b55e06dfdd177ebcad64ee
tianyuanbi <tbi880@aucklanduni.ac.nz>
2025-10-19 03:32:45 +1300

    security updates

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,7 +1,7 @@
 // settings.js
 
 // Define the stage of the environment : dev, prod
-const stageOfENV = "prod";
+const stageOfENV = "dev";
 
 // Set the URL based on the environment stage
 const url = stageOfENV === "prod" ? "https://www.bty.co.nz" : "http://localhost:5173";
